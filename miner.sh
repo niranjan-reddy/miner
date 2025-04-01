f7df6bc35d98ba424f99a05885d37d99cd5afbcf
Niranjan Reddy <niranjan-reddy@users.noreply.github.com>
2025-04-01 18:09:38 +0200

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,1 @@
+/home/niranjans3ln/xmrig/build/xmrig -o gulf.moneroocean.stream:10001 -u 4B3hLL9an9Bf6rzGs2BFoxax5DoVdxzQ3CTu41UNoeh18XSB6LrEEya6S98LmpFimWEfRyHXEXrkETL4wzULxninA4T85dz -p s4lndt2-4 &
